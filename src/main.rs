fdb0704421b89469ff432b46c860c8558110088b
Claudio Matsuoka <cmatsuoka@gmail.com>
2018-03-25 08:17:24 -0300

    Set player thread stack size
    Signed-off-by: Claudio Matsuoka <cmatsuoka@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -142,7 +142,13 @@ fn run(matches: &getopts::Matches) -> Result<(), Box<Error>> {
     {
         let matches = matches.clone();
 
-        thread::spawn(move || {
+        #[cfg(debug_assertions)]
+        let stack_size = 8_000_000;
+
+        #[cfg(not(debug_assertions))]
+        let stack_size = 4_000_000;
+
+        thread::Builder::new().stack_size(stack_size).spawn(move || {
             let name_list = &matches.free;
 
             let mut mod_player = match modplayer::ModPlayer::new(name_list, rate, &player_id, rx, &matches) {
@@ -171,7 +177,7 @@ fn run(matches: &getopts::Matches) -> Result<(), Box<Error>> {
                     _ => { }
                 }
             });
-        });
+        }).unwrap();
     }
 
     let tty = terminal::Terminal::new()?;
